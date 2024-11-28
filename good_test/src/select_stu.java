4b96c98ffe3be1923c9e4d616f7a5346276baf24
Huang <360838210@qq.com>
2024-11-28 18:37:43 +0800

    "第二次学面向对象练习-收集学生信息"

--- good_test/src/select_stu.java
+++ good_test/src/select_stu.java
@@ -1,2 +1,36 @@
 public class select_stu {
+    private String stu_name;
+    private int stu_age;
+    private int stu_id;
+
+    public select_stu(){}
+    public select_stu(String stu_name, int stu_age, int stu_id) {
+        this.stu_name = stu_name;
+        this.stu_age = stu_age;
+        this.stu_id = stu_id;
+    }
+
+    public String getStu_name() {
+        return stu_name;
+    }
+
+    public void setStu_name(String stu_name) {
+        this.stu_name = stu_name;
+    }
+
+    public int getStu_age() {
+        return stu_age;
+    }
+
+    public void setStu_age(int stu_age) {
+        this.stu_age = stu_age;
+    }
+
+    public int getStu_id() {
+        return stu_id;
+    }
+
+    public void setStu_id(int stu_id) {
+        this.stu_id = stu_id;
+    }
 }
