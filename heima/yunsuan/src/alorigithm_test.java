889a06ce4f1f761cbb3c0d1be6c88a77bfffaa54
ht <360838210@qq.com>
2024-12-02 23:01:49 +0800

    开始进入demo小练习-买飞机票

--- heima/yunsuan/src/alorigithm_test.java
+++ heima/yunsuan/src/alorigithm_test.java
@@ -1,4 +1,3 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class alorigithm_test {
@@ -72,32 +71,93 @@ public class alorigithm_test {
 
         /// 程序自动生成一个1-100之间的随机数字，使用程序实现猜出这个数字是多少？
 
-        Random rand = new Random();
-        int number = rand.nextInt(100)+1;
+//        Random rand = new Random();
+//        int number = rand.nextInt(100)+1;
+//
+//        boolean flag = true;
+//        while (flag) {
+//            Scanner input = new Scanner(System.in);
+//            System.out.println("Enter number ");
+//            int guess = input.nextInt();
+//            if (number > guess) {
+//                System.out.println("你猜的比较小");
+//            }
+//            else if (number < guess) {
+//                System.out.println("你猜的比较大");
+//            }
+//            else {
+//                flag = false;
+//                break;
+//            }
+//        }
+//        if (flag = false) {
+//            System.out.println("猜对了");
+//        }
+//        else {
+//            ;
+//        }
 
-        boolean flag = true;
-        while (flag) {
-            Scanner input = new Scanner(System.in);
-            System.out.println("Enter number ");
-            int guess = input.nextInt();
-            if (number > guess) {
-                System.out.println("你猜的比较小");
-            }
-            else if (number < guess) {
-                System.out.println("你猜的比较大");
-            }
-            else {
-                flag = false;
-                break;
-            }
+        /// 复制数组，定义一个方法copyofrange（）；将数组arr中从索引from开始到索引为to的元素，复制到新的数组当中
+//        int[] test_arr = {1,2,3,4,5};
+//        int[] answer = copyofrange(test_arr,1,4);
+//        for (int i = 0; i < answer.length; i++) {
+//            System.out.print(answer[i]+" ");
+//        }
+//
+//    }
+//    public static int[] copyofrange(int[] arr,int from,int to){
+//        int[] newarr = new int[to-from];
+//        int index = 0;
+//        for(int i=from;i<to;i++){
+////            int temp =arr[i];    或者换个思路，重新定义一个新索引
+////            newarr[i-from]=temp;
+//            newarr[index]=arr[i];
+//            index++;
+//        }
+//        return newarr;
+
+        /// 卖飞机票
+        Scanner input = new Scanner(System.in);
+        System.out.print("请输入该机票的价格: ");
+        int price = input.nextInt();
+        input.nextLine();
+        System.out.println("请输入购买机票的月份： ");          ///其实这里为了判断输入的月份是否有效，可以定义一个方法即；在循环中，用while循环和break进行完成
+        int month = input.nextInt();
+        input.nextLine();
+        System.out.println("请选择你所乘坐的座位： ");
+        String choice = input.nextLine();
+        input.close();
+        System.out.println("你订的机票价格为： "+price);
+        System.out.println("你购买的机票月份： "+month);
+        System.out.println("你选择的座位为： "+choice);
+        boolean flag_hotmonth = is_hot_day(month);
+        boolean flag_seated = is_seat_advance(choice);
+        double final_price = 0;
+        if (flag_hotmonth && flag_seated) {
+            final_price = price * 0.9;
+        }else if (flag_hotmonth && !flag_seated) {
+            final_price = price * 0.85;
         }
-        if (flag = false) {
-            System.out.println("猜对了");
+        else if (!flag_hotmonth && flag_seated) {
+            final_price = price * 0.7;
         }
         else {
-            ;
+            final_price = price * 0.65;
         }
+        System.out.println("根据各种条件所致，你最终所需的费用为： "+final_price);
+    }
 
+    public static boolean is_hot_day(int month){
+        if(month >=5 && month <=10){
+            return true;    ///true则为旺季
+        }
+        return false;  ///false 则为淡季
+    }
+    public static boolean is_seat_advance(String seat){
+        if (seat.equals("商务座")){
+            return true;
+        }
+        return false;
     }
 }
 
